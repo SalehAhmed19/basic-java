bc0e9ff6225a22185ada57c34465d1de33f55050
SalehAhmed19 <saleh.ahmed.mahin@gmail.com>
2025-06-05 02:27:46 +0600

    primitive types

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
 public class Main {
     public static void main(String[] args) {
         // variable declaration
-        int age = 30;
-        age = 35;
+        int num = 30;
 
         // copping variable value into the other variable
         int myAge = 23;
         int herAge = myAge;
         System.out.println(herAge);
+
+        // types of data
+        // primitive
+        // 1. byte
+        // 2. short
+        // 3. int
+        // 4. long
+        // 5. float
+        // 6. double
+        // 7. char
+        // 8. boolean
+
+        byte age = 31;
+        int viewsCount = 123_456_789; // for more readability
+        long viewsCount2 = 3_123_456_789L; // L suffix for Long to solve the conflict between int and long
+        float price = 10.99F; // F suffix for Float to solve the conflict between Double and Float
+        char letter = 'A'; // char represents single character
+        String letters = "Apple"; // String represents multiple character
+        boolean isReal = true;
     }
 }
